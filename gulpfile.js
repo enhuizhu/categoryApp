b24406c5baee40a56ab41db8d5911dde33ec4f42
rocabee-david <david@rocabee.com>
2017-08-08 12:14:07 +0100

    update gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,20 +5,38 @@ const source = require('vinyl-source-stream');
 const babelify = require('babelify');
 const uglify = require('gulp-uglify');
 const minify = require('gulp-minify');
+const cleanCSS = require('gulp-clean-css');
+const dest = './dist/';
 
 gulp.task('buildCss', function () {
     return gulp.src('./src/scss/main.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./dist/'));    
+        .pipe(gulp.dest(dest))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest(dest));    
 });
 
 gulp.task('buildJs', () => {
-   return browserify('./src/main.jsx', {debug: true})
+   return browserify('./src/main.jsx', {debug: false})
         .transform(babelify)
+        .plugin("minifyify", {map: false, uglify: {
+            compress: {
+                drop_console: true,
+                dead_code: true,
+                conditionals: true,
+                unused: true,
+                if_return: true,
+                global_defs: {
+                    DEBUG: false
+                }
+            },
+            mangle: true,
+            "screw-ie8": true
+        }})
         .bundle()
         .on('error', function(err) { console.error(err); this.emit('end'); })
         .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest(dest));
 });
 
 gulp.task('watch', () => {
